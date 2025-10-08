a83d8310b02968788ddfce2dc90965e1c0e3dc22
GLevente44 <gecsel@kkszki.hu>
2025-10-08 08:21:25 +0200

    Connection String is created

---
+++ Trader/Connect.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trader
+{
+    internal class Connect
+    {
+        public MySqlConnection connection;
+
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string connectionString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "trader";
+            _user = "root";
+            _password = "";
+
+            connectionString = $"SERVER={_host};DATABASE={_db};UID={_user};PASSWORD={_password};SSlMode=None";
+            
+            connection = new MySqlConnection(connectionString)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+        }
+    }
+}
